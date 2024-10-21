b1da681c986f5d2bc524f1e08767c50a7043c669
Jaylaelike <smarkwisai@gmail.com>
2024-10-21 17:42:40 +0700

    first commit

---
+++ src/data/categories.ts
@@ -0,0 +1,11 @@
+// List of categories for blog posts
+export const CATEGORIES = [
+	'Transmitter',
+	'Electronic && Electrical',
+	'Air Condition',
+	'TIE',
+	'เครื่องมือวัด',
+	'Technology',
+	'Programming',
+	'IOT'
+] as const
